71d6b9b20226a5dfcdd1f304d507d5203546e671
cuongpipe <cuongpipe@gmail.com>
2026-06-12 19:16:12 +0700

    sua header thêm map vô trang chủ v.v

--- SanTheThao/obj/Debug/net10.0/SanTheThao.AssemblyInfo.cs
+++ SanTheThao/obj/Debug/net10.0/SanTheThao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SanTheThao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a732134020c8c4793e5ea75340e93e4d840e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81570314ab7001a01ccf2bb7042a1a94675f4ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("SanTheThao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SanTheThao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
